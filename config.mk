14c401a9160a018bac1ee420e222cd91c8cac43b
Simon Maurer <simon.maurer@unibe.ch>
2024-03-28 16:12:42 +0100

    remove zlog submodule

--- config.mk
+++ config.mk
@@ -6,16 +6,16 @@
 ###############################################################################
 
 # The version number of the box library ($(VMAJ).$(VMIN).$(VREV))
-VMAJ = 0
-VMIN = 10
-VREV = 4
+VMAJ = 1
+VMIN = 0
+VREV = 0
 VDEB = 1
 
 # the RTS library
-LIB_SMXZLOG = -lsmxzlog-0.3
+LIB_SMXZLOG = -lsmxzlog-1.0
 
 # the include path of the smxzlog header files
-INC_SMXZLOG = -I/usr/include/smx/libsmxzlog-0.3
+INC_SMXZLOG = -I/usr/include/smx/libsmxzlog-1.0
 
 # the name of the library
 LIBNAME = smxrts
