a3b10b0d0c219b39f9d3a3c4ab045a9ea9b597c6
Simon Maurer <simon.maurer@unibe.ch>
2025-04-23 13:35:50 +0200

    add comments

--- include/smxtypes.h
+++ include/smxtypes.h
@@ -81,6 +81,7 @@ typedef struct smx_net_sig_s smx_net_sig_t;           /**< ::smx_net_sig_s */
 typedef struct smx_config_data_map_s smx_config_data_map_t;
 /** ::smx_msg_tsmem_data_maps_s */
 typedef struct smx_config_data_maps_s smx_config_data_maps_t;
+/** The source callback function signature */
 typedef void (*smx_source_callback_t)( smx_net_t* net );
 /**
  * The error state of a channel end
@@ -398,11 +399,16 @@ struct smx_net_s
 };
 
 /**
- *
+ * The source channel structure
  */
 struct smx_net_source_s
 {
+    /**
+     * The callback function which is executed before each main loop run of a
+     * net.
+     */
     smx_source_callback_t callback;
+    /** The source channel. */
     smx_channel_t* port;
 };
 
@@ -425,7 +431,7 @@ struct smx_net_sig_s
     } out;                          /**< output channels */
     struct {
         int count;                  /**< the number of connected source ports */
-        /** an array of channel pointers */
+        /** an array of source channel structures */
         smx_net_source_t items[SMX_MAX_SOURCE_CHS];
     } source;                       /**< internal input channel for sources */
 };
